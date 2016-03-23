63aceb4a17e46a3268d775681a306b8ab550aee4
Arialdo Martini <arialdomartini@gmail.com>
2016-03-23 06:46:58 +0100

    Arialdo and Giuseppe's idea

---
+++ AvoidIfV1/AvoidIfV1/ActionOneDelete.cs
@@ -0,0 +1,10 @@
+﻿namespace AvoidIfV1
+{
+    class ActionOneDelete : IBaseAction
+    {
+        public string Run()
+        {
+            return "Action one delete";
+        }
+    }
+}
---
+++ AvoidIfV1/AvoidIfV1/ActionTwoDelete.cs
@@ -0,0 +1,10 @@
+﻿namespace AvoidIfV1
+{
+    class ActionTwoDelete : IBaseAction
+    {
+        public string Run()
+        {
+            return "Action two delete";
+        }
+    }
+}
