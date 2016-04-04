3a69ecd807a66bdb1383fead23dd868ffecc5366
Roberto Robuschi <rrobuschi@gmail.com>
2016-04-04 20:57:54 +0200

    Autofac way to avoid if/switch... sample with a for loop

---
+++ NoIfWithAutofac/NoIfWithAutofac/NoIf.cs
@@ -0,0 +1,40 @@
+﻿namespace NoIfWithAutofac
+{
+    public interface INoIf
+    {
+        string MethodOne(string param1);
+
+        string MethodTwo(string param2);
+    }
+
+    public class NoIfOne : INoIf
+    {
+        public string MethodOne(string param1)
+        {
+            return "NoIfOne_MethodOne";
+        }
+
+        public string MethodTwo(string param2)
+        {
+            return "NoIfOne_MethodTwo";
+        }
+    }
+
+    public class NoIfTwo : INoIf
+    {
+        public string MethodOne(string param1)
+        {
+            return "NoIfTwo_MethodOne";
+        }
+
+        public string MethodTwo(string param2)
+        {
+            return "NoIfTwo_MethodTwo";
+        }
+    }
+
+    public class NoIfTest
+    {
+        
+    }
+}
---
+++ NoIfWithAutofac/NoIfWithAutofac/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Autofac;
+
+namespace NoIfWithAutofac
+{
+    class Program
+    {
+
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            if (args == null) throw new ArgumentNullException(nameof(args));
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(new NoIfOne()).Named<INoIf>("1");
+            builder.RegisterInstance(new NoIfTwo()).Named<INoIf>("2");
+
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                for (var i = 1; i < 3; i++)
+                {
+                    var classInstance = scope.ResolveNamed<INoIf>(i.ToString());
+                    Console.WriteLine(classInstance.MethodOne("aaa"));
+                    Console.WriteLine(classInstance.MethodTwo("bbb"));
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
